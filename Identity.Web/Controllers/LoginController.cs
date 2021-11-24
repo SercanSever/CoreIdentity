f2688e3a1a086446ab9f985d2cd5b1d51ffb3829
Sercan Sever <sercan.sever16@gmail.com>
2021-11-24 15:56:26 +0300

    login

--- Identity.Web/Controllers/LoginController.cs
+++ Identity.Web/Controllers/LoginController.cs
@@ -10,7 +10,7 @@ namespace Identity.Web.Controllers
    {
       private readonly UserManager<User> _userManager;
       private readonly SignInManager<User> _signInManager;
-      public LoginController(UserManager<User> userManager,SignInManager<User> signInManager)
+      public LoginController(UserManager<User> userManager, SignInManager<User> signInManager)
       {
          _userManager = userManager;
          _signInManager = signInManager;
@@ -24,20 +24,24 @@ namespace Identity.Web.Controllers
       [HttpPost]
       public async Task<IActionResult> SignIn(UserViewModel userViewModel)
       {
-          if (ModelState.IsValid)
-          {
-              var user = await _userManager.FindByEmailAsync(userViewModel.Email);
-              if (user!=null)
-              {
-                  await _signInManager.SignOutAsync();
-                  var result = await _signInManager.PasswordSignInAsync(user,userViewModel.Password,false,false);
-                  if (result.Succeeded)
-                  {
-                        return RedirectToAction("Index","Home");
-                  }
-                  return View(userViewModel);
-              }
-          }
+         if (ModelState.IsValid)
+         {
+            var user = await _userManager.FindByEmailAsync(userViewModel.Email);
+            if (user != null)
+            {
+               await _signInManager.SignOutAsync();
+               var result = await _signInManager.PasswordSignInAsync(user, userViewModel.Password, false, false);
+               if (result.Succeeded)
+               {
+                  return RedirectToAction("Index", "Home");
+               }
+               else
+               {
+                  ModelState.AddModelError(nameof(LoginViewModel.Email), "There is no such user");
+               }
+               return View(userViewModel);
+            }
+         }
          return View();
       }
       #endregion
@@ -57,13 +61,16 @@ namespace Identity.Web.Controllers
             user.Email = userViewModel.Email;
             user.PhoneNumber = userViewModel.PhoneNumber;
             var result = await _userManager.CreateAsync(user, userViewModel.Password);
-            if (result.Succeeded){
-                return RedirectToAction("SignIn");
-            }else{
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError("",error.Description);
-                }
+            if (result.Succeeded)
+            {
+               return RedirectToAction("SignIn");
+            }
+            else
+            {
+               foreach (var error in result.Errors)
+               {
+                  ModelState.AddModelError("", error.Description);
+               }
             }
          }
          return View(userViewModel);
