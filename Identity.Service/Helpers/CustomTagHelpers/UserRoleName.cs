b10246e227b9a5b56511d530c8e85f11b820d42a
Sercan Sever <sercan.sever16@gmail.com>
2021-11-30 11:01:37 +0300

    user-role-tag-helper

--- Identity.Service/Helpers/CustomTagHelpers/UserRoleName.cs
+++ Identity.Service/Helpers/CustomTagHelpers/UserRoleName.cs
@@ -1,21 +1,20 @@
+using System.Text;
 using Identity.Entity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace Identity.Service.Helpers.CustomTagHelpers
 {
-   [HtmlTargetElement("td", Attributes = "user-roles")]
+   [HtmlTargetElement("user-roles-tag-helper")]
    public class UserRolesName : TagHelper
    {
+      [HtmlAttributeName("for-userId")]
+      public string UserId { get; set; }
       public UserManager<User> UserManager { get; set; }
-
       public UserRolesName(UserManager<User> userManager)
       {
          this.UserManager = userManager;
       }
-
-      [HtmlAttributeName("user-roles")]
-      public string UserId { get; set; }
 
       public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
       {
@@ -23,14 +22,16 @@ namespace Identity.Service.Helpers.CustomTagHelpers
 
          IList<string> roles = await UserManager.GetRolesAsync(user);
 
-         string html = string.Empty;
+         output.TagMode = TagMode.StartTagAndEndTag;
+
+         var sb = new StringBuilder();
 
          roles.ToList().ForEach(x =>
          {
-            html += $"<span class='badge badge-info'>  {x}  </span>";
+            sb.AppendFormat("<span class='badge badge-info'>  {0}  </span>", x);
          });
 
-         output.Content.SetHtmlContent(html);
+         output.PreContent.SetHtmlContent(sb.ToString());
       }
    }
 }
