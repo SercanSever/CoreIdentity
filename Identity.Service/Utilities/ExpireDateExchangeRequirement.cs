87fe8776389c0fe4c7a557eaec423a0288c798a7
Sercan Sever <sercan.sever16@gmail.com>
2021-12-03 17:36:19 +0300

    expire date change

--- Identity.Service/Utilities/ExpireDateExchangeRequirement.cs
+++ Identity.Service/Utilities/ExpireDateExchangeRequirement.cs
@@ -13,12 +13,14 @@ namespace Identity.Service.Utilities
    {
       protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ExpireDateExchangeRequirement requirement)
       {
-         if (context.User != null && context.User.Identity != null)
+         if (context.User != null & context.User.Identity != null)
          {
             var claim = context.User.Claims.Where(x => x.Type == "ExpireDateExchange" && x.Value != null).FirstOrDefault();
+
             if (claim != null)
             {
                if (DateTime.Now < Convert.ToDateTime(claim.Value))
+
                {
                   context.Succeed(requirement);
                }
